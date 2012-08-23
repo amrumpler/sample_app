83acbaf9826ed68b33fc3881072a7df14359821e
Andy Rumpler <amrumpler@gmail.com>
2012-08-23 11:30:04 -0500

    Add a title helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -16,6 +26,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -29,4 +40,14 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
+
+
 end
